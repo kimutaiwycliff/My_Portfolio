bdda0e81c7fb81e59c154aa8256fd8ce6ca32560
Wycliff Kimutai <kcliff@tutanota.com>
2024-09-05 16:08:00 +0300

    feat(about page):experience and skills

---
+++ src/components/ExperienceItem.jsx
@@ -0,0 +1,54 @@
+const ExperienceItem = ({ jobDetails }) => {
+  const { position } = jobDetails;
+
+  return (
+    <div className="flex justify-between h-48">
+      {position === 'left' ? (
+        <>
+          <ExperienceSection jobDetails={jobDetails} />
+          <ExperienceLine />
+          <div className="w-1/3 "></div>
+        </>
+      ) : (
+        <>
+          <div className="w-1/3 "></div>
+          <ExperienceLine />
+          <ExperienceSection jobDetails={jobDetails} />
+        </>
+      )}
+    </div>
+  );
+};
+
+const ExperienceSection = ({ jobDetails }) => {
+  const { title, description, date, company } = jobDetails;
+  return (
+    <div className="w-1/3 ">
+      {/* JOB TITLE */}
+      <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
+        {title}
+      </div>
+      {/* JOB DESC */}
+      <div className="p-3 text-sm italic">{description} </div>
+      {/* JOB DATE */}
+      <div className="p-3 text-blue-300 text-sm font-semibold">{date} </div>
+      {/* JOB COMPANY */}
+      <div className="p-1 bg-blue-50 text-sm font-semibold w-fit opacity-70">
+        {company}
+      </div>
+    </div>
+  );
+};
+const ExperienceLine = () => {
+  return (
+    <div className="w-1/6 flex justify-center">
+      {/* LINE */}
+      <div className="w-1 h-full bg-gray-600 rounded relative">
+        {/* LINE CIRCLE */}
+        <div className="absolute w-5 h-5 rounded-full ring-4 ring-red-400 bg-white -left-2"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ExperienceItem;
--- src/utils/randomStuff.js
+++ src/utils/randomStuff.js
@@ -5,8 +5,75 @@ export const links = [
   { url: '/contact', title: 'Contact' },
 ];
 export const socials = [
-  { src: '/github.png', title: 'Github', href: '#' },
-  { src: '/linkedin.png', title: 'LinkedIn', href: '#' },
+  {
+    src: '/github.png',
+    title: 'Github',
+    href: 'https://github.com/kimutaiwycliff',
+  },
+  {
+    src: '/linkedin.png',
+    title: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/kimutai-wycliff-698903139/',
+  },
   { src: '/facebook.png', title: 'Facebook', href: '#' },
   { src: '/pinterest.png', title: 'Pinterest', href: '#' },
+];
+
+export const skills = [
+  { name: 'Javascript', level: 90 },
+  { name: 'Typescript', level: 80 },
+  { name: 'React.js', level: 70 },
+  { name: 'Next.js', level: 60 },
+  { name: 'Tailwind CSS', level: 50 },
+  { name: 'MongoDB', level: 40 },
+  { name: 'Express.js', level: 30 },
+  { name: 'Python', level: 20 },
+  { name: 'Node.js', level: 10 },
+  { name: 'Spring Boot', level: 5 },
+  { name: 'GraphQL', level: 50 },
+  { name: 'Framer Motion', level: 40 },
+  { name: 'Three.js', level: 30 },
+  { name: 'Redux', level: 20 },
+  { name: 'React Query', level: 10 },
+  { name: 'Shadcn UI', level: 5 },
+  { name: 'Docker', level: 50 },
+  { name: 'Superbase', level: 40 },
+  { name: 'Git', level: 30 },
+  { name: 'Figma', level: 20 },
+  { name: 'WebGL', level: 10 },
+  { name: 'Vite', level: 5 },
+];
+export const experiences = [
+  {
+    title: 'Senior GIS Desktop Developer',
+    description:
+      'Developed GIS apps, QGIS plugins, and automation scripts; managed data with GeoServer and POSTGIS.',
+    date: '09/2022 - Date',
+    company: 'Konza Silicon Company Limited',
+    position: 'left',
+  },
+  {
+    title: 'Geospatial Engineer',
+    description:
+      'Performed spatial analysis for energy projects; developed automated GIS workflows for efficiency.',
+    date: '02/2022 - 09/2022',
+    company: 'Bell Power International',
+    position: 'right',
+  },
+  {
+    title: 'Lead Surveyor',
+    description:
+      'Conducted topographic surveys and design reviews for the Garaad-Galkayo-Tuurdibi road project.',
+    date: '07/2021 - 02/2022',
+    company: 'KAAD, Somalia',
+    position: 'left',
+  },
+  {
+    title: 'Engineering Survey Intern',
+    description:
+      'Assisted in land surveys, topographic assessments, and CAD designs for the Busia-Malaba road project.',
+    date: '08/2019 - 02/2020',
+    company: 'Kenya National Highways Authority (KeNHA)',
+    position: 'right',
+  },
 ];
